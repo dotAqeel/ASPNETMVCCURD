4168d882666e4eaacc67dcf8fadc8210aa1cab1b
AqeelAhmed <dotaqeel@gmail.com>
2022-12-06 23:47:45 +0500

    Add project files.

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ASPNETMVCCURD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCURD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options):base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
